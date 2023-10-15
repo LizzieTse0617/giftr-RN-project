4f619db7038f36b4b196a24d947f307c29cc8dd9
Lizzie Tse <tse00020@algonquinlive.com>
2023-10-15 16:20:50 -0400

    camera pic returns

--- screens/AddIdeaScreen.js
+++ screens/AddIdeaScreen.js
@@ -3,44 +3,72 @@ import { View, Image, StyleSheet, Keyboard, TouchableWithoutFeedback } from 'rea
 import SaveButton from '../components/SaveButton';
 import CancelButton from '../components/CancelButton';
 import { Text, TextInput } from 'react-native-paper';
-import CameraComponent from '../components/CameraComponent'; // Import your CameraComponent
+import CameraComponent from '../components/CameraComponent';
+import generateUniqueId from '../components/generateUniqueId';
+import { useGlobalDispatch } from '../components/GlobalContext';
 
 export default function AddIdeaScreen({ route, navigation }) {
+  const { personId, personName, capturedImageData } = route.params;
   const [capturedImage, setCapturedImage] = useState(null);
+  const [ideaText, setIdeaText] = useState('');
+  const dispatch = useGlobalDispatch();
 
   const onPictureTaken = (photo) => {
-    console.log('Captured Image URI:', photo.uri);
-    console.log('Captured Image Width:', photo.width);
-    console.log('Captured Image Height:', photo.height);
-    
-    setCapturedImage(photo.uri);
+    setCapturedImage(photo);
   };
 
+  const onSaveButtonPress = () => {
+    const newIdeaId = generateUniqueId();
 
+    // Dispatch an action to add the new idea to a person
+    dispatch({
+      type: 'ADD_IDEA_TO_PERSON',
+      payload: {
+        personId: personId,
+        ideaData: {
+          id: newIdeaId,
+          text: ideaText,
+          img: capturedImage.uri,
+          width: capturedImage.width,
+          height: capturedImage.height,
+        },
+      },
+    });
+
+    navigation.navigate('IdeaScreen', {
+      personId: personId,
+      personName: personName,
+      capturedImageData: capturedImageData,
+      capturedImage: capturedImage,
+      ideaText: ideaText,
+    });
+  };
   return (
-<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-        <Text style={styles.title}>Add an idea</Text>
+        <Text style={styles.title}>Add an idea for {personName}</Text>
         <Text style={styles.text}>Gift</Text>
-        <TextInput style={styles.input} />
+        <TextInput style={styles.input} onChangeText={text => setIdeaText(text)} />
 
-        {/* Use the CameraComponent */}
         <CameraComponent onPictureTaken={onPictureTaken} />
 
         {capturedImage && (
           <View style={styles.imagePreviewContainer}>
-            <Image source={{ uri: capturedImage }} style={styles.imagePreview} />
+            <Image source={{ uri: capturedImage.uri }} style={styles.imagePreview} />
           </View>
         )}
 
         <View style={styles.buttonContainer}>
           <CancelButton title="Cancel" />
-          <SaveButton title="Save" style={styles.btn} />
+          <SaveButton title="Save" style={styles.btn} onPress={onSaveButtonPress} />
         </View>
       </View>
     </TouchableWithoutFeedback>
   );
 }
+
+// Rest of your styles...
+
 
 const styles = StyleSheet.create({
   container: {
--- screens/IdeaScreen.js
+++ screens/IdeaScreen.js
@@ -1,38 +1,60 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Image, Button } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-export default function IdeaScreen({route, navigation}){
-    const { personId, personName } = route.params;
-    const insets = useSafeAreaInsets();
-    
-    return (
 
-<View style={[styles.container, { paddingTop: insets.top }]}>
-        <Text style={styles.title}> Idea for <Text style={styles.personName}>{personName}</Text> </Text>
-        
-  
-        </View>
-    )
+export default function IdeaScreen({ route, navigation }) {
+  const { personName, personId, capturedImageData, capturedImage,ideaText } = route.params || {};
+  const insets = useSafeAreaInsets();
+
+  const navigateToAddIdea = () => {
+    navigation.navigate('AddIdea', {
+      personId,
+      personName,
+      capturedImageData,
+      capturedImage,
+      ideaText
+    });
+  };
+  console.log(capturedImage,ideaText,capturedImage.uri)
+  //console.log('capturedImage URI:', capturedImage.uri);
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+    <Text style={styles.title}>Idea for <Text style={styles.personName}>{personName}</Text></Text>
+    {capturedImage && (
+      <View style={{ backgroundColor: 'white' }}>
+        <Text>{ideaText}</Text>
+        <Image source={{ uri: capturedImage.uri }} style={styles.image} />
+      </View>
+    )}
+    <Button title="Add Idea" onPress={navigateToAddIdea} />
+  </View>
+  );
 }
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 20,
-        width: '100%',
-        backgroundColor:'#FFFFFF'
-      },
-      title: {
-        fontSize: 35,
-        fontWeight: 'bold', 
-      },
-      personName: {
-        color: '#7C56FF', // You can apply your custom style to personName here
-        fontWeight: 'bold', 
-      },
-      text: {
-        paddingTop: 10,
-        fontSize: 14,
-        color:'#393939',
-        paddingBottom:5
-      },  
-})
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  personName: {
+    color: '#7C56FF',
+    fontWeight: 'bold',
+  },
+  text: {
+    paddingTop: 10,
+    fontSize: 14,
+    color: '#393939',
+    paddingBottom: 5,
+  },
+  image:{
+    width:300,
+    height:200
+  }
+});
